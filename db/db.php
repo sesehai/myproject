651c679e5187bc5740955009f7dbf4957b447fcc
unknown <luqi@luqi-007.(none)>
2012-04-04 00:39:48 +0800

    new file:   .cache/.dataModel 	new file:   .project 	new file:   .settings/org.eclipse.php.core.projectOptions.prefs 	new file:   common.php 	new file:   date/Date_Holidays.php 	new file:   db/db.php 	new file:   http/http.php 	new file:   log/log.php 	new file:   os/Guess.php 	new file:   phpdoc/Phpdoc.php

---
+++ db/db.php
@@ -0,0 +1,33 @@
+<?php
+define('ROOT_PATH',dirname(dirname(__FILE__)));
+require_once ROOT_PATH.'/common.php';
+
+require_once 'DB.php';
+$DbManager = new DB();
+$db = $DbManager->factory($type="mysqli", $options = false);
+
+
+$dsn = array(
+      'phptype'  => 'mysqli',
+      'username' => 'mclient_readonly',
+      'password' => '!HY%wn&#*nse',
+      'hostspec' => '60.28.199.202',
+      'database' => 'mclient',
+      'key'      => '',
+      'cert'     => '',
+      'ca'       => '',
+      'capath'   => '',
+      'cipher'   => '',
+);
+
+$options = array(
+      'ssl' => true,
+);
+$db->connect($dsn, $persistent = false);
+$query="select * from mclient_vrsupdate limit 5";
+$rows = $db->simpleQuery($query);
+$rowsAry = array();
+while($db->fetchInto($rows, &$rowsAry, $fetchmode='', $rownum = null)){
+    print_r($rowsAry);
+}
+?>
---
+++ http/http.php
@@ -0,0 +1,8 @@
+<?php
+require_once 'common.php';
+
+require_once 'HTTP/Client.php';
+
+//HTTP_Client::post ($url, mixed $data [, boolean $preEncoded = FALSE [, array $files = array()]]);
+
+?>
