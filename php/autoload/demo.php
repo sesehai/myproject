ee8b28302c01b6d97aa2876f3fd18fb8bca9544b
路奇 <sesehai@gmail.com>
2018-01-06 19:10:26 +0800

    test

--- php/autoload/demo.php
+++ php/autoload/demo.php
@@ -5,11 +5,15 @@ require_once 'base.php';
 spl_autoload_register(array('Base','auto_load'));
 
 $mobile = new Mobile();
-echo $mobile->getName(),"<br>";
+echo $mobile->getName(),"\n";
 
 
 $android_phone = new Android_Phone();
-echo $android_phone->getName(),"<br>";
+echo $android_phone->getName(),"\n";
 
 $android_pad = new Android_Pad();
-echo $android_pad->getName(),"<br>";
+echo $android_pad->getName(),"\n";
+
+echo "\n";
+echo $argv[1];
+var_dump($argv);
